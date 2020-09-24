20333e50d467371da811a41cb081c0e6023a3d99
Inagaki <days5382@yahoo.co.jp>
2020-09-24 09:09:06 +0000

    PHP/Laravelコース No19課題提出

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,6 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
