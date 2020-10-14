238a5234619c25da9938348d07178d891714badd
Inagaki <days5382@yahoo.co.jp>
2020-10-14 06:21:28 +0000

    PHP/Laravel 19 一般ユーザーが読むニュースサイトを作成しよう

--- routes/web.php
+++ routes/web.php
@@ -41,5 +41,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+//Route::get('/', 'NewsController@index');
+
+Route::get('profile.', 'ProfileController@index');
 
 ?>
